4ff063b95168be35523bb2d042d74cb97e98888e
Nathan Pena <nathanjaypena2@gmail.com>
2026-08-10 22:03:40 -0500

    Add: Email insights API endpoint

---
+++ backend/src/main/java/com/leadzen/backend/dto/EmailRequest.java
@@ -0,0 +1,6 @@
+package com.leadzen.backend.dto;
+
+public record EmailRequest(String leadName, String emailDate, String emailSubject, String emailBody) { 
+        
+}
+    
--- backend/src/main/java/com/leadzen/backend/service/EmailService.java
+++ backend/src/main/java/com/leadzen/backend/service/EmailService.java
@@ -1,9 +1,21 @@
 package com.leadzen.backend.service;
 
+import org.springframework.stereotype.Service;
+import com.leadzen.backend.dto.EmailRequest;
+
+@Service
 public class EmailService {
 
-    public record GetTest(String getResponse) { 
-        
+    public String getSummary(EmailRequest lead){
+        return lead.emailBody();
+    }
+
+    public String getUrgency(EmailRequest lead){
+        return lead.emailSubject();
+    }
+
+    public String getReply(EmailRequest lead){
+        return lead.leadName();
     }
 
 }
